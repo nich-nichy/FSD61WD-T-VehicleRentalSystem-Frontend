05dc9a86a59b99d0eb53b9f1d9db893f1eacbd2b
Mohamed Nishath M <nishathmohamed786@gmail.com>
2024-09-21 14:03:19 +0530

    Implementing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <div>
+        <Navbar />
         <p className='text-2xl text-gray-500'>Hello World</p>
       </div>
     </>
